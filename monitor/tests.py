90e107ec4c2dee50555c1a7716a541202fb44dcb
Kegan Gan <kegan@kebook.local>
2009-10-31 15:45:41 +0800

    Updated tests and fixtures.

--- monitor/tests.py
+++ monitor/tests.py
@@ -97,41 +97,33 @@ class MonitonTestCase(TestCase):
         self.assertTemplateUsed(response, 'monitor/information.html')
         self.assertEquals(response.context['moniton'], Moniton.objects.get(pk=1))
 
-    # def test_monitor_initial_log_creation(self):
-    #     """
-    #     Test creating of first log, when not log data is available.
-    #     """
-    #     Log.objects.all().delete()
-    #     call_command("send_all", '1238860800') # Timestamp for 2009-04-05.
-    #     log = Log.objects.all().latest('created_at')
-    #
-    #     self.assertEquals(log.start, log.end)
-    #
-    # def test_monitor_subsequest_log_creation(self):
-    #     """
-    #     Test creating of subsequent log entry.
-    #     """
-    #     call_command("send_all", '1238860800') # Timestamp for 2009-04-05.
-    #     log = Log.objects.all().latest('created_at')
-    #
-    #     self.assertEquals(log.start, datetime(2009, 04, 05, 0, 0))
-    #     self.assertTrue(log.start < log.end)
-    #
-    # def test_monitor_notification(self):
-    #     """
-    #     Test the correctness of notifications.
-    #     """
-    #     call_command("send_all", '1238860800') # Timestamp for 2009-04-05.
-    #
-    #     self.assertEquals(len(mail.outbox), 2)
-    #
-    #     self.assertEquals(mail.outbox[0].to, ['confirm1@example.com'])
-    #     self.assertNotEquals(mail.outbox[0].body.rfind('Terrible Crime Middle In Date'), -1)
-    #     self.assertNotEquals(mail.outbox[0].body.rfind('Terrible Crime East'), -1)
-    #
-    #     self.assertEquals(mail.outbox[1].to, ['confirm2@example.com'])
-    #     self.assertNotEquals(mail.outbox[1].body.rfind('Terrible Crime Middle In Date'), -1)
-    #     self.assertNotEquals(mail.outbox[1].body.rfind('Terrible Crime West'), -1)
+    def test_notification(self):
+        """
+        Test the correctness of notifications.
+        """
+        call_command("send_all", 2009, 1, 3, 0, 0, 0) # 2009-01-03 midnight
+
+        self.assertEquals(len(mail.outbox), 3)
+
+        self.assertEquals(mail.outbox[0].to, ['kegan@example.com'])
+        self.assertNotEquals(mail.outbox[0].body.rfind('Terrible Crime Middle In Date'), -1)
+        self.assertNotEquals(mail.outbox[0].body.rfind('Terrible Crime East'), -1)
+        self.assertNotEquals(mail.outbox[0].body.rfind('Terrible Crime West'), -1)
+
+        self.assertEquals(mail.outbox[1].to, ['zul@example.com'])
+        self.assertNotEquals(mail.outbox[1].body.rfind('Terrible Crime Middle In Date'), -1)
+        self.assertNotEquals(mail.outbox[1].body.rfind('Terrible Crime East'), -1)
+
+        self.assertEquals(mail.outbox[2].to, ['leremy@example.com'])
+        self.assertNotEquals(mail.outbox[2].body.rfind('Terrible Crime Middle In Date'), -1)
+        self.assertNotEquals(mail.outbox[2].body.rfind('Terrible Crime West'), -1)
+
+    def test_notification_none(self):
+        """
+        Test the correctness of notifications with no crimes in last 24 hours.
+        """
+        call_command("send_all", 2009, 1, 20, 0, 0, 0) # 2009-01-20 midnight
+        self.assertFalse(mail.outbox)
 
     def tearDown(self):
         pass
