bec97356df74d4c85955ca73e2450d705a71249d
Kegan Gan <kegan@kebook.local>
2009-12-18 20:54:50 +0800

    Updated mimetype for javascript file.

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     url(r'^recent/commented/$', recent_commented, name='main-recent-commented'),
     url(r'^search/$', direct_to_template, {'template': 'main/search.html'}, name='main-search'),
 
-    url(r'^icons.js$', direct_to_template, {'template': 'main/icons.js', 'mimetype': 'text/javascript'}, name='main-icons-js'),
+    url(r'^icons.js$', direct_to_template, {'template': 'main/icons.js', 'mimetype': 'application/javascript'}, name='main-icons-js'),
     url(r'^robots.txt$', direct_to_template, {'template': 'main/robots.txt', 'mimetype': 'text/plain'}),
     url(r'^sitemap.xml$', sitemap, {'sitemaps': sitemaps}),
 
