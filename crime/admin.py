023cba2b82150ed25654c610a178821fcb8fc5ef
Kegan <kegan@kegan.info>
2010-08-16 21:27:32 +0800

    Updated crime admin with better columns selection.

--- crime/admin.py
+++ crime/admin.py
@@ -4,7 +4,8 @@ from models import Crime
 
 
 class CrimeAdmin(admin.ModelAdmin):
-    list_filter = ('date', 'updated_at')
-    ordering = ('headline',)
+    list_filter   = ('date', 'updated_at')
+    ordering      = ('-created_at',)
+    list_display  = ('headline', 'author', 'date', 'created_at', 'is_removed')
     search_fields = ('headline',)
 admin.site.register(Crime, CrimeAdmin)
