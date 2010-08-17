f20b0a7708fda7f513eaabe850173ed5de856197
Kegan <kegan@kegan.info>
2010-08-17 22:28:16 +0800

    Added handling for HTTP_X_FORWARDED_FOR.

--- settings_default.py
+++ settings_default.py
@@ -60,6 +60,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'malaysiacrime.SetRemoteAddrFromForwardedFor',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
