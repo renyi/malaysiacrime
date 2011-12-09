941fc5bb9ca708f6bf59b836fa88b2daa1fc64c0
Kegan Gan <kegan@kebook.local>
2011-12-09 16:39:13 +0800

    Disabled CSRF token.

--- settings_default.py
+++ settings_default.py
@@ -61,7 +61,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'malaysiacrime.SetRemoteAddrFromForwardedFor',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
